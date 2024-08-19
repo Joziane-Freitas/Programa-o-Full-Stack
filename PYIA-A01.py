a0373927910ef9014737c91244d4a3cf2e1165a4
Joziane-Freitas <167653871+Joziane-Freitas@users.noreply.github.com>
2024-08-19 17:39:17 -0300

    Add files via upload

---
+++ PYIA-A01.py
@@ -0,0 +1,9 @@
+# Criação da lista de frutas
+frutas = ["maçã", "banana", "laranja", "uva", "abacaxi", "manga"]
+
+# Conversão da lista em uma tupla
+frutas_tupla = tuple(frutas)
+
+# Exibição do conteúdo da tupla
+frutas_tupla
+print(frutas_tupla)
