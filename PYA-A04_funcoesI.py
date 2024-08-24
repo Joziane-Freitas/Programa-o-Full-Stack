a0f22bd0549b2cab1efe6ce3cceaad7dbfb25d33
Joziane-Freitas <167653871+Joziane-Freitas@users.noreply.github.com>
2024-08-24 14:33:17 -0300

    Add files via upload

---
+++ PYA-A04_funcoesI.py
@@ -0,0 +1,7 @@
+def media(numero1, numero2, numero3):
+    soma = numero1 + numero2 + numero3
+    media_aritmetica = soma / 3
+    return media_aritmetica
+
+resultado = media(50, 60, 70)
+print("A média é:", resultado)
